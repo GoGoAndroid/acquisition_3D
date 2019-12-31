85385fc845157846e5daa7a582740e97e6811cf0
GoGoAndroid <sgoyet@yahoo.fr>
2019-12-31 12:45:19 +0100

    before remving opacity changes

--- points_to_stl/takePoints.js
+++ points_to_stl/takePoints.js
@@ -1,39 +1,56 @@
 
 
 let indicePoint=0;
+var x0_image=0;
+var y0_image=0;
+let scale=0;
 
+function log(text){
+  document.getElementById("log").innerHtml=document.getElementById("log").innerHtml+"<br>"+text
+}
 function loadImage(i, path){
 
   var c = document.getElementById(`canvas_${i}`);
-  var ctx = c.getContext("2d");
 
+  /*
+  var ctx = c.getContext("2d");
   var img = new Image();
   img.onload = function() {
-  var scale = Math.min(c.width / img.width, c.height / img.height);
-  var x = (c.width / 2) - (img.width / 2) * scale;
-  var y = (c.height / 2) - (img.height / 2) * scale;
-  ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+  scale = Math.min(c.width / img.width, c.height / img.height);
+  //x0_image = (c.width / 2) - (img.width / 2) * scale;
+  //y0_image = (c.height / 2) - (img.height / 2) * scale;
+  ctx.drawImage(img,0,0, img.width * scale, img.height * scale);
 
   };
-  img.src = path;
-
+  */
+  //c.src = path;
   c.addEventListener("click", clicked, false);
 }
 
+
+function setText(id,text,x,y){
+
+  var div = document.createElement("div");
+  div.style.position= "absolute";
+  div.style.top= y;
+  div.style.left= x;
+  div.style.background = "red";
+  div.style.color = "white";
+  div.innerHTML = text;
+  div.class='id'
+  document.body.appendChild(div);
+}
 
 
 function clicked(evt){
 
   var e = evt.target;
   var dim = e.getBoundingClientRect();
-  var x = evt.clientX - dim.left;
-  var y = evt.clientY - dim.top;
+  console.log(evt)
+  var x = (evt.pageX ) ;
+  var y = (evt.pageY) ;
 
-  let indice=prompt(indicePoint,indicePoint)
-  indicePoint=indice;
-  document.getElementById("log").innerText=
-  document.getElementById("log").innerText+x+" "+y+"\n";
+  setText(evt.id, ""+indicePoint++,x,y)
 
-  evt.target.getContext("2d").fillText(""+indicePoint++, x, y )
 
 }
